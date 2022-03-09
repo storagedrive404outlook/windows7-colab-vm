759031f43158a53a67bf43da639a9752878e5a76
storagedrive404outlook <99749949+storagedrive404outlook@users.noreply.github.com>
2022-03-09 20:10:17 +0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
+./ngrok authtoken $CRP 
+nohup ./ngrok tcp 5900 &>/dev/null &
+echo Please wait for installing...
+sudo apt update -y > /dev/null 2>&1
+echo "Installing QEMU (2-3m)..."
+sudo apt install qemu-system-x86 qemu qemu-system curl -y > /dev/null 2>&1
+echo Downloading Windows Disk...
+curl -L -o win7.zip https://az792536.vo.msecnd.net/vms/VMBuild_20180102/VirtualBox/IE11/IE11.Win7.VirtualBox.zip
+unzip win7.zip
+
+echo "Windows 10 x64 Lite On Google Colab"
+echo Your VNC IP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "Note: Use Right-Click Or Ctrl+C To Copy"
+echo "Please Keep Colab Tab Open, Maximum Time 12h"
+echo Script by Storagedrive404outlook
+sudo qemu-system-x86_64 -vnc :0 -hda lite7.qcow2  -smp cores=2  -m 8192M -machine usb=on -device usb-tablet > /dev/null 2>&1

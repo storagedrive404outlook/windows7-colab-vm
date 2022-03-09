308497c0449db07920f85098281b7ede5a7e3244
storagedrive404outlook <99749949+storagedrive404outlook@users.noreply.github.com>
2022-03-09 20:27:43 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,10 @@ read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
 nohup ./ngrok tcp 5900 &>/dev/null &
 echo Please wait for installing...
 sudo apt update -y > /dev/null 2>&1
+clear
 echo "Installing QEMU (2-3m)..."
 sudo apt install qemu-system-x86 qemu qemu-system curl -y > /dev/null 2>&1
+clear
 echo Downloading Windows Disk...
 curl -L -o win7.zip https://az792536.vo.msecnd.net/vms/VMBuild_20180102/VirtualBox/IE11/IE11.Win7.VirtualBox.zip
 unzip win7.zip
@@ -17,6 +19,7 @@ mv 'IE11 - Win7-disk001.vmdk' win7disk1.vmdk
 rm win7.ova
 mv 'IE11 - Win7.ovf' win7.ovf
 rm win7.ovf
+clear
 echo "Windows 7 x64 Lite On Google Colab"
 echo Your VNC IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'

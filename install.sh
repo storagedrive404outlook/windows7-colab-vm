687284757a3f02c91440451b7dd7d294e6938df1
storagedrive404outlook <99749949+storagedrive404outlook@users.noreply.github.com>
2022-03-09 20:19:58 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,11 +10,14 @@ sudo apt install qemu-system-x86 qemu qemu-system curl -y > /dev/null 2>&1
 echo Downloading Windows Disk...
 curl -L -o win7.zip https://az792536.vo.msecnd.net/vms/VMBuild_20180102/VirtualBox/IE11/IE11.Win7.VirtualBox.zip
 unzip win7.zip
-
-echo "Windows 10 x64 Lite On Google Colab"
+mv 'IE11 - Win7.ova' win7.ova
+rm win7.zip
+tar -xvf win7.ova
+mv 'IE11 - Win7-disk001.vmdk' win7disk1.vmdk
+echo "Windows 7 x64 Lite On Google Colab"
 echo Your VNC IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "Note: Use Right-Click Or Ctrl+C To Copy"
 echo "Please Keep Colab Tab Open, Maximum Time 12h"
 echo Script by Storagedrive404outlook
-sudo qemu-system-x86_64 -vnc :0 -hda lite7.qcow2  -smp cores=2  -m 8192M -machine usb=on -device usb-tablet > /dev/null 2>&1
+sudo qemu-system-x86_64 -vnc :0 -hda win7disk1.vmdk  -smp cores=2  -m 8192M -machine usb=on -device usb-tablet > /dev/null 2>&1
